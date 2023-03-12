78e5b9f28974a0fe0547bb29c516df2fda3f0400
hyy <hyy@pop-os.localdomain>
2023-03-12 13:40:20 -0500

    remove lualine

--- nvim/.config/nvim/lua/yh/packer.lua
+++ nvim/.config/nvim/lua/yh/packer.lua
@@ -66,8 +66,4 @@ return require('packer').startup(function(use)
         end
     }
 
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
 end)
